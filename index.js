424b2766ec945663fc3667d8bba9ec35eb6863d3
bradleyhop <bradleyhop@gmail.com>
2017-07-27 12:44:53 -0500

    Changed multiline strings to template literals for card component building

--- index.js
+++ index.js
@@ -51,20 +51,19 @@ function makeCards(cardContent) {
     var div = "";
 
     for (let i = 0; i < cardContent.length; i++) {
-
-      // template for card html
-      //  includes bootstrap grid settings
-        div += "<div class='card col-sm-5'>" +
-            "<img class='card-img-top' " +
-            "src='" + cardContent[i].screencap + "' alt='" +
-            cardContent[i].alt + "'/>" +
-            "<div class='card-block'>" +
-            "<h4 class='card-title'>" + cardContent[i].cardTitle + "</h4>" +
-            "<p class='card-text'>" + cardContent[i].desc + "</p>" +
-            "<a href='" + cardContent[i].link + "' target='_blank'" +
-            " class='btn'>Go see!</a>" +
-            "</div>" +
-            "</div>";
+        // template for card html
+        //  includes bootstrap grid settings
+        div +=
+            `<div class='card col-sm-5'>
+            <img class='card-img-top' src=' ${cardContent[i].screencap} ' alt='
+            ${cardContent[i].alt}'/>
+                <div class='card-block'>
+                <h4 class='card-title'> ${cardContent[i].cardTitle} </h4>
+                <p class='card-text'> ${cardContent[i].desc}  </p>
+                <a href=' ${cardContent[i].link} target='_blank'
+                class='btn'>Go see!</a>
+                </div>
+            </div>`;
     }
 
     return div;
