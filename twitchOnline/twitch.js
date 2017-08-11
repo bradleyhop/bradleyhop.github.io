4c89cb05cfbab9137ed1ceea2cc4dd784a5a36a7
bradleyhop <bradleyhop@gmail.com>
2017-08-10 22:01:13 -0500

    fixed form style, changed to template literal
    updated materialize cdn

--- twitchOnline/twitch.js
+++ twitchOnline/twitch.js
@@ -17,14 +17,15 @@ var myStreams = ["stockstream", "gamesdonequick", "noopkat", "bdx_inc",
 function notExistingDisplay(usr) {
     // show that the user doesn't exist
     let notExist =
-        "<div class='resultItem card error-color hoverable'>" +
-        "<div class='card-content text-primary-color'>" +
-        "<div class='valign-wrapper'>" +
-        "<i class='material-icons errorIcon'>error</i>" +
-        "<span class='card-title'>" +
-        usr.display_name + "</span></div>" +
-        "<p class='secondary-text-color'>" + usr.notFound + "</p>"  +
-        "</div></div>";
+        `<div class='resultItem card error-color hoverable'>
+            <div class='card-content text-primary-color'>
+                <div class='valign-wrapper'>
+                    <i class='material-icons errorIcon'>error</i>
+                    <span class='card-title'>${ usr.display_name }</span>
+                </div>
+                <p class='secondary-text-color'>${ usr.notFound }</p>
+            </div>
+        </div>`;
 
     $("#all").append(notExist);
     $("#notExist").append(notExist);
@@ -34,21 +35,22 @@ function notExistingDisplay(usr) {
 function notOnlineDisplay(usr) {
     // show user/channel info and appropriate html
     let notOnline =
-        "<div class='resultItem card dark-primary-color hoverable'>" +
-        "<div class='card-content white-text'>";
+        `<div class='resultItem card dark-primary-color hoverable'>
+        <div class='card-content white-text'>`;
     if (usr.logoLink) {
-        notOnline += "<div class='valign-wrapper'>" +
-            "<img class='circle cardImg' src='" + usr.logoLink + "'/>" +
-            "<span class='card-title'>" + usr.display_name +
-            "</span></div>";
+        notOnline += `<div class='valign-wrapper'>
+            <img class='circle cardImg' src='${ usr.logoLink }'/>
+            <span class='card-title'>${ usr.display_name }</span>
+            </div>`;
     } else {
-        notOnline += "<span class='card-title'>" + usr.display_name + "</span>";
+        notOnline += `<span class='card-title'>${ usr.display_name }</span>`;
     }
     if (usr.bio) {
-        notOnline += "<p>" + usr.bio + "</p>";
+        notOnline += `<p>${ usr.bio }</p>`;
     }
-    notOnline += "</div><div class='card-action notOnlineLink'><a href='" + usr.page +
-        "'>Go To Channel</a>" + "</div></div>";
+    notOnline += `</div><div class='card-action notOnlineLink'>
+        <a href='${ usr.page }'>Go To Channel</a>
+        </div></div>`;
 
     $("#all").append(notOnline);
     $("#offline").append(notOnline);
@@ -58,29 +60,29 @@ function notOnlineDisplay(usr) {
 function onlineDisplay(usr) {
     // user/channel info and current stream
     let curStream =
-        "<div class='resultItem card online-color-dark hoverable'>";
+        `<div class='resultItem card online-color-dark hoverable'>`;
     if (usr.online.preview) {
-        curStream += "<div class='card-image'><img src='" +
-            usr.online.preview + "'></div>";
+        curStream += `<div class='card-image'><img src='
+            ${usr.online.preview}'>
+            </div>`;
     }
-    curStream += "<div class='card-content white-text'>";
+    curStream += `<div class='card-content white-text'>`;
     if (usr.logoLink) {
-        curStream += "<div class='valign-wrapper'>" +
-            "<img class='circle cardImg' src='" + usr.logoLink + "'/>" +
-            "<span class='card-title'>" + usr.display_name +
-            "</span></div>";
+        curStream += `<div class='valign-wrapper'>
+            <img class='circle cardImg' src='${ usr.logoLink }'/>
+            <span class='card-title'>${ usr.display_name }
+            </span></div>`;
     } else {
-        curStream += "<span class='card-title'>" + usr.display_name +
-            "</span>";
+        curStream += `<span class='card-title'>${ usr.display_name }</span>`;
     }
     if (usr.bio) {
-        curStream += "<p>" + usr.bio + "</p>";
+        curStream += `<p>${ usr.bio }</p>`;
     }
-    curStream += "<br><p class='streamInfo'>" + usr.online.content + " -<br>" +
-        usr.online.status + "</p>";
-    curStream += "</div><div class='card-action'>" +
-        "<a href='" + usr.online.linkToStream + "'>" +
-        "Go To Stream</a>" + "</div></div>";
+    curStream += `<br><p class='streamInfo'>${ usr.online.content } -<br>
+        ${ usr.online.status }</p>`;
+    curStream += `</div><div class='card-action'>
+        <a href='${ usr.online.linkToStream }'>Go To Stream</a>
+        </div></div>`;
 
     $("#all").append(curStream);
     $("#online").append(curStream);
@@ -94,9 +96,9 @@ $(document).ready(function() {
     let allStreams = streams.concat(nonStreams, myStreams);
     console.log(allStreams.length);
 
-    let progressBar = "<div class='progress'>" +
-        "<div class='indeterminate'></div>" +
-        "</div>";
+    let progressBar = `<div class='progress'>
+        <div class='indeterminate'></div>
+        </div>`;
     $(".indicator").html(progressBar);
 
     // build are usr object and push them onto our array
@@ -146,8 +148,8 @@ $(document).ready(function() {
                     typeof tmpObj.online !== null) {
                     onlineDisplay(tmpObj);
                 } else {
-                    console.log("Something is wrong with calling the display" +
-                        "functions: " + tmpObj.online);
+                    console.log(`Something is wrong with calling the display
+                        functions: ${ tmpObj.online }`);
                 }
 
             }).done( () => {
