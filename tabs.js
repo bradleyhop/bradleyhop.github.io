32b728c1ed29544f2674f893b355a231a3a0c318
bradleyhop <bradleyhop@gmail.com>
2019-10-01 20:09:13 -0500

    active project tab now more obvious

--- tabs.js
+++ tabs.js
@@ -1,9 +1,12 @@
-function openBranch(projects) {
-  let x = document.getElementsByClassName("project-branch");
+function openBranch(evt, projects) {
+    let x = document.getElementsByClassName("project-branch");
+    let tab = document.getElementsByClassName("tab-button");
 
-  for (let i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
+    for (let i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+        tab[i].className = tab[i].className.replace(" tab-active", "");
+    }
 
-  document.getElementById(projects).style.display = "block";
+    document.getElementById(projects).style.display = "block";
+    evt.currentTarget.className += " tab-active";
 }
