e9421b319e20578067cce5fd77ff867e749f81b0
bradleyhop <bradleyhop@gmail.com>
2019-10-06 17:44:12 -0500

    refactor, update using es6

--- randomQuotes/random.js
+++ randomQuotes/random.js
@@ -1,45 +1,36 @@
-function getQuote() {
-    /* Having to use a CORS proxy b/c of insecure json, and Chrome
-     * security features. A bandaid for copepen.
-     * Impossible for me to fix because server providing json isn't
-     * configured for CORS.
-     */
-    $.getJSON('https://cors-anywhere.herokuapp.com/http://quotes.stormconsultancy.co.uk/quotes/random.json', function(json) {
-        quote = json.quote;
-        author = json.author;
-
-        $('.quotelives').html("<h2>" + quote + "<br>-- " + author + "<br><br><br>Press ENTER for new quote or 'T' to share this quote via Twitter.</h2>");
-    });
-}
-
-// Trim quote down for twitter
-function twitterChop(currentQuote) {
-    if (currentQuote.length > 65) {
-        return currentQuote.slice(0, 65) + "...";
-    }
-}
+var quote, author;
 
 $(document).keypress(function(event) {
 
-    /* Get new quote when 'enter'
-     * key is pressed
-     */
-    if (event.which === 13) {
-        getQuote();
-    }
-    /* Send quote to twitter when
-     * 'T' or 't' is pressed
-     */
-    if (event.which === 84 || event.which === 116) {
-        var twitterFriendly = '"' + quote + '..."' + ' - ' + author;
+  if (event.key === "Enter") {
+    $.getJSON(
+      "http://quotes.stormconsultancy.co.uk/quotes/random.json",
+      json => {
+        quote = json.quote;
+        author = json.author;
+      }
+    ).then(() =>
+      $(".quotelives").html(
+        `<h2>${quote}<br>--  ${author}<br><br><br>Press ENTER for new quote or 'T' to share this quote via Twitter.</h2>`
+      )
+    );
+  }
 
-        if (typeof quote != 'undefined' || typeof author != 'undefined') {
-            quote = twitterChop(quote);
-
-            window.open('https://twitter.com/intent/tweet?hashtags=randomQuoteGenerator&url=https://bit.ly/1THoBDa&related=freecodecamp&text=' + encodeURIComponent('"' + quote + '"' + ' - ' + author));
-        } else {
-            $('#quoteLives').empty().append("<h2>No quote has been fetched, yet!<br>Please click below to get a quote.</h2>");
-        }
-        window.open('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent(twitterFriendly));
+  if (event.key === "T" || event.key === "t") {
+    if (typeof quote !== "undefined" || typeof author !== "undefined") {
+      if (quote.length > 65) {
+        quote = quote.slice(0, 65) + "...";
+      }
+      window.open(
+        "https://twitter.com/intent/tweet?hashtags=randomQuoteGenerator,freecodeCamp&url=https://bit.ly/1THoBDa&related=freecodecamp&text=" +
+          encodeURIComponent(`"${quote}" - ${author}`)
+      );
+    } else {
+      $(".quoteLives")
+        .empty()
+        .append(
+          "<h2>No quote has been fetched, yet!<br>Please press ENTER to get a quote.</h2>"
+        );
     }
+  }
 });
