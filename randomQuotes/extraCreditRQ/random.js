6941956bccbd144ef2af3b60d72bbc00b2989db1
bradleyhop <bradleyhop@gmail.com>
2019-10-07 15:15:42 -0500

    rip out any bootstrap, improper tag use, fix css, start to rip out jquery

--- randomQuotes/extraCreditRQ/random.js
+++ randomQuotes/extraCreditRQ/random.js
@@ -1,35 +1,36 @@
 var quote, author;
 
-$(document).keypress(function(event) {
-
-  if (event.key === "Enter") {
+document.addEventListener("keypress", function(event) {
+  if (event.key === 'Enter') {
     $.getJSON(
-      "http://quotes.stormconsultancy.co.uk/quotes/random.json",
+      'http://quotes.stormconsultancy.co.uk/quotes/random.json',
       json => {
         quote = json.quote;
         author = json.author;
       }
-    ).then(() =>
-      $(".quotelives").html(
-        `<h2>${quote}<br>--  ${author}<br><br><br>Press ENTER for new quote or 'T' to share this quote via Twitter.</h2>`
+    )
+      .then(() =>
+        $('.quotelives').html(
+          `<p>${quote}<br>--  ${author}<br><br><br>Press ENTER for new quote or 'T' to share this quote via Twitter.</p>`
+        )
       )
-    );
+      .catch(error => console.log(error));
   }
 
-  if (event.key === "T" || event.key === "t") {
-    if (typeof quote !== "undefined" || typeof author !== "undefined") {
-      if (quote.length > 65) {
-        quote = quote.slice(0, 65) + "...";
+  if (event.key === 'T' || event.key === 't') {
+    if (typeof quote !== 'undefined' || typeof author !== 'undefined') {
+      if (quote.length > 180) {
+        quote = quote.slice(0, 180) + '...';
       }
       window.open(
-        "https://twitter.com/intent/tweet?hashtags=randomQuoteGenerator,freecodeCamp&url=https://bit.ly/1THoBDa&related=freecodecamp&text=" +
+        'https://twitter.com/intent/tweet?hashtags=randomQuoteGenerator,freecodeCamp&url=https://bit.ly/1THoBDa&related=freecodecamp&text=' +
           encodeURIComponent(`"${quote}" - ${author}`)
       );
     } else {
-      $(".quoteLives")
+      $('.quoteLives')
         .empty()
         .append(
-          "<h2>No quote has been fetched, yet!<br>Please press ENTER to get a quote.</h2>"
+          '<p>No quote has been fetched, yet!<br>Please press ENTER to get a quote.</p>'
         );
     }
   }
