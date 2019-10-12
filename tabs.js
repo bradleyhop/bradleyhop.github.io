34fe84bead9b6075eef6d958b8f6be69120f60eb
bradleyhop <bradleyhop@gmail.com>
2019-10-11 21:11:19 -0500

    remove loop for readability, use Object.entries

--- tabs.js
+++ tabs.js
@@ -1,12 +1,12 @@
 function openBranch(evt, projects) {
-    let x = document.getElementsByClassName("project-branch");
-    let tab = document.getElementsByClassName("tab-button");
+  let branch = document.getElementsByClassName('project-branch');
+  let tab = document.getElementsByClassName('tab-button');
 
-    for (let i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-        tab[i].className = tab[i].className.replace(" tab-active", "");
-    }
+  Object.entries(branch).map(el => (el[1].style.display = 'none'));
+  Object.entries(tab).map(
+    el => (el[1].className = el[1].className.replace(' tab-active', ''))
+  );
 
-    document.getElementById(projects).style.display = "block";
-    evt.currentTarget.className += " tab-active";
+  document.getElementById(projects).style.display = 'block';
+  evt.currentTarget.className += ' tab-active';
 }
