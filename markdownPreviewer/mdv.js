fdeb71e9f41fbc5650db297f3e10cc94df324802
bradleyhop <bradleyhop@gmail.com>
2019-12-26 12:11:10 -0600

    default markdown as string literal; styling

--- markdownPreviewer/mdv.js
+++ markdownPreviewer/mdv.js
@@ -1,20 +1,39 @@
 var vm = new Vue({
   el: '.flex-container',
   data: {
-    editor: '# Heading1',
-    usermd: '',
+    // default markdown on pageload
+    usermd: `
+# Heading1
+## Heading2
+
+[Bradley's portfolio](https://bradleyhop.github.io)
+
+\`\`\`
+var let a = 52;
+a += 5;
+console.log(a);
+\`\`\`
+
+1. List item
+2. Second list item
+
+> This is a blockquote. It is really, really long.
+> It makes these words look important!
+
+Here's an image:
+![alt text](https://secure.meetupstatic.com/photos/event/4/c/b/b/600_468259643.jpeg 'free code camp logo')
+
+This is made **bold** with asterisk, this __one__ with double underscore.`,
     preview: ''
   },
   methods: {
     translate() {
-      if (this.usermd !== '') {
-        this.preview = marked(this.usermd);
-      } else {
-        this.preview = marked(this.editor);
-      }
+      this.preview = marked(this.usermd,
+        { pedantic: false, gfm: true, breaks: true, smartLists: true,
+          xhtml: false });
     }
   },
   mounted () {
     this.translate();
-  }
+  },
 });
