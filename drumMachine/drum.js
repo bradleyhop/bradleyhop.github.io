1f33c8d4af9990e0b454e686bf19c267b6cf8b22
bradleyhop <bradleyhop@gmail.com>
2020-01-12 22:27:54 -0600

    listen for keyup and fire method; correct display implementation

--- drumMachine/drum.js
+++ drumMachine/drum.js
@@ -1,15 +1,20 @@
 var dm = new Vue({
   el: "#drum-machine",
-  created: {
-    function () {
-      window.addEventListener('keyup', this.hitDrum);
-    }
+  data: {
+    whatDrum:  ""
   },
   methods: {
     hitDrum: function (inst) {
+      inst = inst.toUpperCase();
       if (['Q', 'W', 'E', 'A', 'S', 'D', 'Z', 'X', 'C'].indexOf(inst) >= 0) {
-        console.log("you hit the: " + inst);
+        console.log( "you hit the: " + inst);
       }
     }
+  },
+  created: function () {
+    let vm = this;
+    window.addEventListener("keyup", function(event) {
+      vm.hitDrum(event.key);
+    });
   },
 });
