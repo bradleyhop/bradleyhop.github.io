a1473c8c6c18a6b2e868cf8707f2657cec7a6c6c
bradleyhop <bradleyhop@gmail.com>
2020-04-02 11:44:32 -0500

    npm audit fix; app scales with text-only zoom; lint fixes

--- drum-machine/src/App.vue
+++ drum-machine/src/App.vue
@@ -25,10 +25,14 @@ export default {
 </script>
 
 <style lang="scss">
-// Note: the original Game Boy has a resolution of 160 x 144, so much of the
-//  scaling is done to that resolution.
+// Note: the original Game Boy has a resolution of 160px x 144px, so much of the
+//  scaling is done to that resolution. Converted to rem using 20px as base.
+
+$width-gameboy: 8rem;
+$height-gameboy: 7.2rem;
 
 $responsive-width: 599px;
+
 $light-grey: #bfbcb7;
 $deep-grey: #555362;
 $spinach-screen: #94c11f;
@@ -39,14 +43,17 @@ $spinach-screen: #94c11f;
   src: url("../public/assets/Pixeled.ttf");
 }
 
+html {
+  font-family: 'Pixelated', monospace;
+  font-size: 20px;
+}
+
 body {
   align-items: center;
   background-color: $light-grey;
   background-image: none;
   box-shadow: none;
   display: flex;
-  font-family: 'Pixelated', monospace;
-  font-size: 20px;
   height: 100vh;
   justify-content: center;
   margin: 0;
@@ -61,12 +68,12 @@ body {
 
 // next four rules needed for the focus-visible polyfill; gives focus on keyboard navigation only
 
-:focus {
-  outline:none;
+::-moz-focus-inner {
+  border: 0;
 }
 
-::-moz-focus-inner {
-  border:0;
+:focus {
+  outline: none;
 }
 
 .js-focus-visible :focus:not(.focus-visible) {
@@ -74,7 +81,7 @@ body {
 }
 
 .focus-visible {
-  border: #fff solid 2px;
+  border: #fff solid 0.1rem;
   border-radius: 50%;
 }
 
@@ -83,24 +90,24 @@ body {
 #app-container {
   background: $light-grey;
   border: none;
-  border-radius: 9px 9px 50px 9px;
-  height: calc(4.5 * 144px);
+  border-radius: 0.45rem 0.45rem 2.5rem 0.45rem;
+  height: calc(4.5 * #{$height-gameboy});
   padding: 1rem;
   text-align: center;
-  width: calc(2.5 * 160px);
+  width: calc(2.5 * #{$width-gameboy});
 
   @media only screen and (min-width: $responsive-width) {
-    border: 2px solid $deep-grey;
+    border: 0.1rem solid $deep-grey;
   }
 }
 
 .wrapper-display {
   background: $deep-grey;
-  border-radius: 9px 9px 50px 9px;
-  height: calc(1.8 * 144px);
+  border-radius: 0.45rem 0.45rem 2.5rem 0.45rem;
+  height: calc(1.8 * #{$height-gameboy});
   margin: auto;
-  padding-top: calc(0.25 * 144px);
-  width: calc(2.25 * 160px);
+  padding-top: calc(0.25 * #{$height-gameboy});
+  width: calc(2.25 * #{$width-gameboy});
 }
 
 .power-light {
@@ -113,26 +120,26 @@ body {
   border-radius: 50%;
   border-style: none;
   float: left;
-  height: 12px;
-  margin-left: 25px;
-  margin-top: 60px;
-  width: 12px;
+  height: 0.6rem;
+  margin-left: 1.25rem;
+  margin-top: 3rem;
+  width: 0.6rem;
 }
 
 #display {
   align-items: center;
   background: $spinach-screen;
-  border-radius: 5px;
+  border-radius: 0.25rem;
   display: flex;
-  height: calc(1.5 * 144px);
+  height: calc(1.5 * #{$height-gameboy});
   justify-content: center;
-  margin-left: calc(0.4 * 160px);
-  width: calc(1.5 * 160px);
+  margin-left: calc(0.4 * #{$width-gameboy});
+  width: calc(1.5 * #{$width-gameboy});
 }
 
 .speaker-grill {
   float: right;
-  padding-right: 10px;
+  padding-right: 0.5rem;
 
   @media only screen and (min-width: $responsive-width) {
     padding: 0;
@@ -141,23 +148,23 @@ body {
 
 .speaker-holes {
   background: $deep-grey;
-  border-radius: 2px;
+  border-radius: 0.1rem;
   display: inline-block;
   height: 3.5rem;
-  margin-right: 25px;
+  margin-right: 1.25rem;
   transform: rotate(-40deg);
-  width: 4px;
+  width: 0.2rem;
 }
 
 .sh1 {
-  margin-top: 10px;
+  margin-top: 0.5rem;
 }
 
 .sh2 {
-  margin-top: 20px;
+  margin-top: 1rem;
 }
 
 .sh3 {
-  margin-top: 30px;
+  margin-top: 1.5rem;
 }
 </style>
